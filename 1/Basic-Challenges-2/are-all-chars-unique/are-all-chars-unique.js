2f0952fb0b34a80ab5c45522463b4d87e4e79939
Romulus Andrei Bacila <30293758+Tolsi111@users.noreply.github.com>
2024-03-20 14:23:08 +0200

    are all chars unique

---
+++ 1/Basic-Challenges-2/are-all-chars-unique/are-all-chars-unique.js
@@ -0,0 +1,13 @@
+function areAllCharactersUnique(str) {
+  const charSet = new Set();
+
+  for (let i = 0; i < str.length; i++) {
+    if (charSet.has(str[i])) {
+      return false;
+    }
+    charSet.add(str[i]);
+  }
+  return true;
+}
+
+module.exports = areAllCharactersUnique;
